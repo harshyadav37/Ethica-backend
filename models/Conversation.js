f97e2c5a3cb52bfb71e303cb3891a3f42c303417
harshyadav37 <hy050712@gmail.com>
2026-06-08 18:07:06 +0530

    done message

--- models/Conversation.js
+++ models/Conversation.js
@@ -5,7 +5,7 @@ const conversationSchema = new mongoose.Schema(
     participants: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
+        ref: "SOCIAL",
         required: true,
       },
     ],
